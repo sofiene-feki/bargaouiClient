3205d8fc25809e35f4512b5f3c778eb3909fd21a
sofiene-feki <sofian.feki@gmail.com>
2025-09-05 20:42:55 +0200

    test

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -324,17 +324,34 @@ export default function ProductDetails() {
     }
     navigate("/shop"); // redirect to shop page
   };
-
   const formatPrice = (price) => {
     return new Intl.NumberFormat("fr-TN", {
       style: "currency",
       currency: "TND",
       minimumFractionDigits: 3,
-    }).format(price);
+    })
+      .format(price)
+      .replace(/\b(TND|DT)\b/g, "د.ت");
   };
+  const [shrink, setShrink] = useState(false);
+
+  useEffect(() => {
+    let lastShrink = false;
+
+    const handleScroll = () => {
+      const shouldShrink = window.scrollY > 0; // start shrinking immediately
+      if (shouldShrink !== lastShrink) {
+        setShrink(shouldShrink);
+        lastShrink = shouldShrink;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="py-15 md:py-20 px-2 ">
+    <div className="py-12 md:py-20 ">
       {user && (
         <div className="flex  bg-white max-w-7xl mx-auto items-center justify-between border-b border-gray-200 pb-2  mb-6">
           {/* Center title */}
@@ -407,23 +424,29 @@ export default function ProductDetails() {
           </div>
         </div>
       )}
-      <div className="max-w-7xl mx-auto lg:flex lg:gap-12">
+      <div className="max-w-7xl mx-auto lg:flex lg:gap-12 ">
         {/* LEFT: Media gallery */}
         {loading ? (
           <div className=" w-full h-[400px] lg:w-1/2 md:mb-6  lg:mb-0 bg-gray-200 rounded-lg animate-pulse"></div>
         ) : (
-          <ProductMediaGallery
-            media={product?.media} // pass media array directly
-            selectedMedia={selectedMedia} // currently selected
-            onSelectMedia={setSelectedMedia} // when clicking thumbnail
-            onAddMedia={handleFileUpload} // upload handler
-            onDeleteMedia={deleteMedia} // delete handler
-            isEditable={isEdit || isCreate} // edit/create flag
-          />
+          <div className={` sticky top-12 z-10 `}>
+            {" "}
+            <ProductMediaGallery
+              media={product?.media}
+              selectedMedia={selectedMedia}
+              onSelectMedia={setSelectedMedia}
+              onAddMedia={handleFileUpload}
+              onDeleteMedia={deleteMedia}
+              isEditable={isEdit || isCreate}
+              shrink={shrink}
+              minHeight={250}
+              maxHeight={500}
+            />
+          </div>
         )}
 
         {/* RIGHT: Product Info */}
-        <div className="w-full lg:w-1/2 mt-4 lg:mt-0">
+        <div className="w-full lg:w-1/2 mt-[30px] lg:mt-0 px-2" dir="rtl">
           {/* Name, Price, Description */}
           {isEdit || isCreate ? (
             <>
@@ -431,36 +454,38 @@ export default function ProductDetails() {
             </>
           ) : (
             <>
-              {loading ? (
-                <div className=" h-8 mb-2 w-3/4 bg-gray-200 rounded-lg animate-pulse"></div>
-              ) : (
-                <h1 className="text-2xl  break-words bg-clip-text  drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] font-bold text-gray-900 sm:text-3xl mb-2">
-                  {product.Title}
-                </h1>
-              )}
-
-              {loading ? (
-                <div className=" h-8 mb-2 w-1/4 bg-gray-200 rounded-lg animate-pulse"></div>
-              ) : (
-                <p className="text-3xl md:flex border-b border-gray-200  justify-between font-bold break-words bg-clip-text drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] text-gray-900 mb-3">
-                  <span>{formatPrice(product.Price)}</span>
-                  <span className="flex items-center gap-2">
-                    {product.Quantity > 0 ? (
-                      <span className="text-green-600 text-xs font-semibold">
-                        En stock
+              <div className=" sticky md:static top-[370px] shadow-xl z-10 bg-white">
+                {" "}
+                {loading ? (
+                  <div className=" h-8 mb-2 w-3/4 bg-gray-200 rounded-lg animate-pulse"></div>
+                ) : (
+                  <h1 className="text-2xl  break-words bg-clip-text  drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] font-bold text-gray-900 sm:text-3xl">
+                    {product.Title}
+                  </h1>
+                )}
+                {loading ? (
+                  <div className=" h-8 mb-2 w-1/4 bg-gray-200 rounded-lg animate-pulse"></div>
+                ) : (
+                  <p className="text-xl flex border-b border-gray-200 mb-5 justify-between font-bold break-words bg-clip-text drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] text-gray-900">
+                    <span>{formatPrice(product.Price)}</span>
+                    <span className="flex items-start mt-2 gap-2">
+                      {product.Quantity > 0 ? (
+                        <span className="text-green-600 text-xs font-semibold">
+                          En stock
+                        </span>
+                      ) : (
+                        <span className="text-red-500 text-xs line-through">
+                          Rupture de stock
+                        </span>
+                      )}
+                      <FaShippingFast className="text-[#2c2d84] md:w-6 md:h-6 w-4 h-4" />
+                      <span className="text-xs text-[#2c2d84]">
+                        Livraison rapide
                       </span>
-                    ) : (
-                      <span className="text-red-500 text-xs line-through">
-                        Rupture de stock
-                      </span>
-                    )}
-                    <FaShippingFast className="text-[#2c2d84] md:w-6 md:h-6 w-5 h-5 ml-3" />
-                    <span className="text-xs text-[#2c2d84]">
-                      Livraison rapide
                     </span>
-                  </span>
-                </p>
-              )}
+                  </p>
+                )}
+              </div>
 
               <div className="mb-4">
                 {" "}
