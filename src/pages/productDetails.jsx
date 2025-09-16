83f52585c05e0d6fdf7fe649e92a8ada00e74a08
sofiene-feki <sofian.feki@gmail.com>
2025-09-16 17:28:12 +0200

    corrct price

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -123,12 +123,22 @@ export default function ProductDetails() {
     }
   }, [product]);
 
+  const originalPrice = product?.Price;
+  const promotion = product?.promotion || 0; // percentage
+  const discountedPrice = +(
+    originalPrice -
+    (originalPrice * promotion) / 100
+  ).toFixed(2);
+  const savings = +(originalPrice - discountedPrice).toFixed(2);
+
   const handleAddToCart = () => {
+    const finalPrice = promotion > 0 ? discountedPrice : originalPrice;
+
     dispatch(
       addItem({
         productId: product._id,
         name: product.Title,
-        price: product.Price,
+        price: finalPrice, // ✅ use discounted or original
         image: selectedMedia?.src,
         selectedSize: selectedSize?.name ?? null,
         selectedSizePrice: selectedSize?.price ?? null,
@@ -137,6 +147,7 @@ export default function ProductDetails() {
         sizes: product.sizes,
       })
     );
+
     dispatch(openCart());
   };
 
@@ -467,7 +478,27 @@ export default function ProductDetails() {
                 <div className="h-8 mb-2 w-1/4 bg-gray-200 rounded-lg animate-pulse"></div>
               ) : (
                 <p className="md:text-3xl text-xl flex border-b border-gray-200 justify-between font-bold break-words bg-clip-text drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] text-gray-900 py-2 mb-3">
-                  <span>{formatPrice(product.Price)}</span>
+                  {product?.promotion > 0 ? (
+                    <div className="flex items-end gap-2">
+                      <div className="relative flex items-start">
+                        <h3 className="text-2xl md:text-4xl font-bold text-[#87a736] leading-none">
+                          {discountedPrice}{" "}
+                        </h3>
+                        <span className="absolute -top-1 right-0 translate-x-full text-sm font-semibold text-gray-700">
+                          DT
+                        </span>
+                      </div>
+
+                      <span className="line-through text-gray-400 text-xl md:ml-0 ml-3 mt-1">
+                        {originalPrice}{" "}
+                      </span>
+                      <span className="text-xs text-red-500 font-semibold">
+                        Économisez {savings} DT
+                      </span>
+                    </div>
+                  ) : (
+                    <span>{formatPrice(product.Price)}</span>
+                  )}
                   <span className="flex items-center gap-2">
                     {product.Quantity > 0 ? (
                       <span className="text-green-600 text-xs font-semibold">
